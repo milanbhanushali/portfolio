492c2fa91d52519eb734637f41dcec93555c929d
Jobanputra Rushi <61899855+rushijobanputra@users.noreply.github.com>
2021-11-14 23:37:30 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,63 @@
+
+window.addEventListener("load",()=>{
+    document.querySelector(".main").classList.remove("hidden");
+    document.querySelector(".home-selection").classList.add("active");
+    /*------page loader--------*/
+    document.querySelector(".page-loader").classList.add("fade-out");
+    setTimeout(()=>{
+        document.querySelector(".page-loader").style.display = "none";
+    },600);
+});
+
+
+/*----toggle navbar-----*/
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click",()=>{
+    hideSection();
+    toggleNavbar();
+    document.body.classList.toggle("hide-scrolling");
+});
+
+function hideSection()
+{
+    console.querySelector("selection.active").classList.toggle("fade-out");
+}
+function toggleNavbar()
+{
+    document.querySelector(".header").classList.toggle("active");
+}
+
+/*--------Active Section-----------------*/
+document.addEventListener("click",(e)=>{
+    if(e.target.classList.contains("link-item") && e.target.hash !== "" )
+    {
+        //active the overlay to prevent multiple clicks
+        document.querySelector(".overlay").classList.add("active");
+        navToggler.classList.add("hide");
+       // const hash = e.target.hash;
+        if(e.target.classList.contains("nav-item"))
+        {
+            toggleNavbar();
+        }
+        else
+        {
+            //console.log("false");
+            hideSection();
+            document.body.classList.add("hide-scrolling");
+        }
+        setTimeout(()=>{
+            document.querySelector("selection.active").classList.remove("active","fade-out");
+            document.querySelector(e.target.hash).classList.add("active");
+            window.scrollTo(0,0);
+            document.body.classList.remove("hide-scrolling");
+            navToggler.classList.remove("hide");
+            document.querySelector(".overlay").classList.remove("active");
+
+        },500);
+
+    }
+});
+
 /*------------------------About Table-----------------------*/
 const tabsContainer = document.querySelector(".about-tabs");
 aboutSection = document.querySelector(".about-section");
@@ -59,3 +119,5 @@ function portfolioItemDetails(portfolioItem)
 
 
 }
+
+/*----toggle navbar-----*/
