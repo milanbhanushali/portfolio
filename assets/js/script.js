a63fe0872a5f10cb964562ff24d929e41c6a5f42
milanbhanushali <60770889+milanbhanushali@users.noreply.github.com>
2021-11-16 15:56:01 +0530

    Semi Final

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,36 +32,36 @@ function toggleNavbar()
 //#endregion
 
 //#region  active section
-// /*--------Active Section-----------------*/
-// document.addEventListener("click",(e)=>{
-//     if(e.target.classList.contains("link-item") && e.target.hash !== "" )
-//     {
-//         //active the overlay to prevent multiple clicks
-//         document.querySelector(".overlay").classList.add("active");
-//         navToggler.classList.add("hide");
-//        // const hash = e.target.hash;
-//         if(e.target.classList.contains("nav-item"))
-//         {
-//             toggleNavbar();
-//         }
-//         else
-//         {
-//             //console.log("false");
-//             hideSection();
-//             document.body.classList.add("hide-scrolling");
-//         }
-//         setTimeout(()=>{
-//             document.querySelector("selection.active").classList.remove("active","fade-out");
-//             document.querySelector(e.target.hash).classList.add("active");
-//             window.scrollTo(0,0);
-//             document.body.classList.remove("hide-scrolling");
-//             navToggler.classList.remove("hide");
-//             document.querySelector(".overlay").classList.remove("active");
+/*--------Active Section-----------------*/
+document.addEventListener("click",(e)=>{
+    if(e.target.classList.contains("link-item") && e.target.hash !== "" )
+    {
+        //active the overlay to prevent multiple clicks
+        document.querySelector(".overlay").classList.add("active");
+        navToggler.classList.add("hide");
+       // const hash = e.target.hash;
+        if(e.target.classList.contains("nav-item"))
+        {
+            toggleNavbar();
+        }
+        else
+        {
+            //console.log("false");
+            hideSection();
+            document.body.classList.add("hide-scrolling");
+        }
+        setTimeout(()=>{
+            document.querySelector("section.active").classList.remove("active","fade-out");
+            document.querySelector(e.target.hash).classList.add("active");
+            window.scrollTo(0,0);
+            document.body.classList.remove("hide-scrolling");
+            navToggler.classList.remove("hide");
+            document.querySelector(".overlay").classList.remove("active");
 
-//         },500);
+        },500);
 
-//     }
-// });
+    }
+});
 
 //#endregion
 
