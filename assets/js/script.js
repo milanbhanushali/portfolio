50ee30346607dda79d0573ce8871622f6ba48fe0
milanbhanushali <60770889+milanbhanushali@users.noreply.github.com>
2021-11-16 12:44:48 +0530

    fonts naikha & js changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,34 +60,7 @@ console.log("Haale che")
 
 /*------------------------ About Tabs -----------------------*/
 
-// let expereince = document.getElementById("experience");
-// let education = document.getElementById("education");
-// let btnExp = document.getElementById("btnExperience");
-// let btnEdu = document.getElementById("btnEducation");
-// function educationClick(){
-    
-//     console.log("Education hailu");
 
-//     expereince.classList.remove("active");
-//     education.classList.add("active");
-
-//     btnEdu.classList.add("active");
-
-//     btnExp.classList.remove("active");
-    
-
-// }
-// function expereinceClick(){
-    
-//     console.log("Expereince hailu");
-    
-//     education.classList.remove("active");
-//     expereince.classList.add("active");
-    
-//     btnExp.classList.add("active");
-//     btnEdu.classList.remove("active");
-    
-// }
 let tabsContainer = document.querySelector(".about-tabs"),
 aboutSection = document.querySelector(".about-section");
 
@@ -109,52 +82,53 @@ tabsContainer.addEventListener("click",(e)=>{
 
 
 /*---------------portflolio Item Details popup-----------------------*/
-// document.addEventListener("click",(e)=>{
-//     if(e.target.classList.contains("view-project-btn"))
-//     {
-//         togglePortfolioPopup()
-//         document.querySelector(".portfolio-popup").scrollTo(0,0);
-//         portfolioItemDetails(e.target.parentElement);
-//     }
-// });
+document.addEventListener("click",(e)=>{
+    if(e.target.classList.contains("view-project-btn"))
+    {
+        console.log("HI");
+        togglePortfolioPopup()
+        document.querySelector(".portfolio-popup").scrollTo(0,0);
+        portfolioItemDetails(e.target.parentElement);
+    }
+});
 
-// function togglePortfolioPopup()
-// {
-//     document.querySelector(".portfolio-popup").classList.toogle("open");
-//     document.body.classList.toggle("hide-scrolling");
-//     document.querySelector(".main").classList.toggle("fade-out");
-// }
+function togglePortfolioPopup()
+{
+    document.querySelector(".portfolio-popup").classList.toggle("open");
+    document.body.classList.toggle("hide-scrolling");
+    document.querySelector(".main").classList.toggle("fade-out");
+}
 
-// document.querySelector(".pp-close".addEventListener("click",togglePortfolioPopup));
-
-
-// // hide popup when clicking outside of it
-// document.addEventListener("click",(e)=>{
-//     if(e.target.classList.contains("pp-inner"))
-//     {
-//         togglePortfolioPopup();
-//     }
-// });
+document.querySelector(".pp-close".addEventListener("click",togglePortfolioPopup));
 
 
-// function portfolioItemDetails(portfolioItem)
-// {
-//     //console.log(portfolioItem);
-//     document.querySelector(".pp-thumbnail img").src =
-//     portfolioItem.querySelector(".portfolio-item-thumbnail img").src;
-
-//     document.querySelector(".pp-header h3").innerHTML = 
-//     portfolioItem.querySelector(".portfolio-item-title").innerHTML;
-
-
-//     document.querySelector(".pp-body").innerHTML = 
-//     portfolioItem.querySelector(".portfolio-item-details").innerHTML;
+// hide popup when clicking outside of it
+document.addEventListener("click",(e)=>{
+    if(e.target.classList.contains("pp-inner"))
+    {
+        togglePortfolioPopup();
+    }
+});
 
 
-// }
+function portfolioItemDetails(portfolioItem)
+{
+    //console.log(portfolioItem);
+    document.querySelector(".pp-thumbnail img").src =
+    portfolioItem.querySelector(".portfolio-item-thumbnail img").src;
+
+    document.querySelector(".pp-header h3").innerHTML = 
+    portfolioItem.querySelector(".portfolio-item-title").innerHTML;
 
 
-const ctx = document.getElementById('myChart');
+    document.querySelector(".pp-body").innerHTML = 
+    portfolioItem.querySelector(".portfolio-item-details").innerHTML;
+
+
+}
+
+
+    const ctx = document.getElementById('myChart');
     const myChart = new Chart(ctx, {
         type: 'bar',
         data: {
